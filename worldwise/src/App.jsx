094d3166e175a6bb890b6f32b172d56993083fe5
manishkhulbe <manishkhulbe123@gmail.com>
2023-07-28 17:47:15 +0530

    react router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,10 +1,67 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import AppLayout from "./pages/AppLayout";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
+
+const BASE_URl = "http://localhost:8000";
 function App() {
-  const a = 90
+
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URl}/cities`);
+        const data = await res.json()
+        setCities(data);
+      } catch (error) {
+        alert('There was an error loading data...')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[])
+
   return (
-    <div>
-      app
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
