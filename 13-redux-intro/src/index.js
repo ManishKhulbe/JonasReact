259ba6cb90b59a9e9638828de52c09b2cec29def
manishkhulbe <manishkhulbe123@gmail.com>
2023-08-07 18:45:41 +0530

    redux-toolkit intro

--- 13-redux-intro/src/index.js
+++ 13-redux-intro/src/index.js
@@ -3,7 +3,7 @@ import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './store';
+import store from './store-v2';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
